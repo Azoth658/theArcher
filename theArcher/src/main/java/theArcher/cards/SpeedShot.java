4b67236cb418835e6e03e61b2714b37cef4f3133
Azoth658 <Azoth658@gmail.com>
2021-10-25 11:44:04 +0100

    Added SpeedShot card

--- theArcher/src/main/java/theArcher/cards/SpeedShot.java
+++ theArcher/src/main/java/theArcher/cards/SpeedShot.java
@@ -2,74 +2,61 @@ package theArcher.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.ExhaustAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import theArcher.actions.FatigueAction;
-import theArcher.actions.PreparedAction;
 import theArcher.characters.TheArcher;
 
 import static theArcher.TheArcher.CustomTags.SHOT;
 import static theArcher.TheArcher.makeCardPath;
 
-public class PreparedShot extends AbstractPreparedCard {
+public class SpeedShot extends AbstractFatigueCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = theArcher.TheArcher.makeID(PreparedShot.class.getSimpleName());
+    public static final String ID = theArcher.TheArcher.makeID(SpeedShot.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
-    public static final String IMG = makeCardPath("preparedShot.png");
+    public static final String IMG = makeCardPath("speedShot.png");
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 
     // /TEXT DECLARATION/
 
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheArcher.Enums.COLOR_ORANGE;
 
-    private static final int COST = 0;
-    private static final int DAMAGE = 4;
-    private static final int PREPARED = 4;
-    private static final int PREPARED_UPGRADE = 2;
-
-    private static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    private static final int COST = 2;
+    private static final int DAMAGE = 20;
+    private static final int FATIGUE = 10;
     private static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
 
     // /STAT DECLARATION/
 
-    public PreparedShot() {
+    public SpeedShot() {
         super(ID, NAME, IMG, COST,DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
         this.baseMisc = this.misc = DAMAGE;
         this.baseDamage = this.damage = this.misc;
-        this.preparedAmount = PREPARED;
-        this.magicNumber = PREPARED;
+        this.fatigueAmount = FATIGUE;
+        this.magicNumber = FATIGUE;
         this.baseMagicNumber = magicNumber;
-        linkPreparedAmountToMagicNumber = true;
-        this.preparedEffect = EXTENDED_DESCRIPTION[0];
+        this.fatigueEffect = EXTENDED_DESCRIPTION[0];
         tags.add(SHOT);
-    }
-
-    @Override
-    public void triggerOnEndOfPlayerTurn(){
-        this.addToBot(new PreparedAction(this));
-    }
-    @Override
-    public void onRetained(){
-        this.addToBot(new PreparedAction(this));
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+        this.addToBot(new FatigueAction(this));
+        this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
     }
 
     @Override
@@ -86,10 +73,9 @@ public class PreparedShot extends AbstractPreparedCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            this.upgradeDamage(2);
-            this.upgradeMisc(2);
-            this.preparedAmount += PREPARED_UPGRADE;
-            this.magicNumber = preparedAmount;
+            this.upgradeDamage(10);
+            this.upgradeMisc(10);
+            this.magicNumber = fatigueAmount;
             this.baseMagicNumber = magicNumber;
             initializeDescription();
         }
